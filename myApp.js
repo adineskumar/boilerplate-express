b5400c00486427d493cff1d2c83bc3899973c7d1
adineskumar <adineskumar.isds@gmail.com>
2023-06-17 15:40:44 +0000

    remove app.listen()

--- myApp.js
+++ myApp.js
@@ -2,8 +2,6 @@ let express = require('express');
 let app = express();
 const port = process.env.PORT || 6000
 
-app.listen(port)
-
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 })
