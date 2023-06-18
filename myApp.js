8fe94f47998d581276058d68b40efc619e94ce0d
adineskumar <adineskumar.isds@gmail.com>
2023-06-18 04:39:49 +0000

    updated styless

--- myApp.js
+++ myApp.js
@@ -1,9 +1,12 @@
 let express = require('express');
 let app = express();
-const port = process.env.PORT || 6000
+app.use(express.static(__dirname + '/public/'));
+
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 })
+
+
 
 module.exports = app;
