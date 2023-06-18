770fb37f35a351858b0caf74280b923a50585022
adineskumar <adineskumar.isds@gmail.com>
2023-06-18 05:00:46 +0000

    Added /json path

--- myApp.js
+++ myApp.js
@@ -7,6 +7,10 @@ app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 })
 
+app.get('/json', function (request, response) {
+  response.json({"message": "Hello json"})
+})
+
 
 
 module.exports = app;
